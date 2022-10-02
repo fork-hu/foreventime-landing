1d740597d1e379eede1a5b0017f240edbf12a344
Zoltan <zoltan.sisak@gmail.com>
2022-10-02 21:18:05 +0200

    Presenter refactoring

--- src/sections/Presenters/Presenters.jsx
+++ src/sections/Presenters/Presenters.jsx
@@ -79,7 +79,7 @@ const Presenters = (props) => {
 	const [presenterText] = useStaticElement("speaker") 
 	const [allPresenters] = useAllElements("presenters")
 	const [showAll, setShowAll] = useState(false)
-	const notHighlightedPresenter = allPresenters.filter(s => !s.highlighted)
+	const notHighlightedPresenter = allPresenters?.filter(s => !s.highlighted)
 
     return (
         <Section id="eloadok" container placeholder>
@@ -89,13 +89,17 @@ const Presenters = (props) => {
 			<Text subtitle>
 				<StructuredText data={presenterText} />
 			</Text>
-            
-			
-			{(allPresenters) && <HighlightedPresenters presenters={allPresenters.filter(s => s.highlighted)} />}
-			{(notHighlightedPresenter.length > 0) && 
-					<Button onClick={() => setShowAll(!showAll)}><Arrow rotation={showAll ? 180 : 0} /> További előadóink </Button>
+            {(allPresenters) && 
+				<>
+					<HighlightedPresenters presenters={allPresenters.filter(s => s.highlighted)} />
+					{(notHighlightedPresenter.length > 0) && 
+						<>
+							<Button onClick={() => setShowAll(!showAll)}><Arrow rotation={showAll ? 180 : 0} /> További előadóink </Button>
+							{(allPresenters && showAll) && <AllPresenters presenters={notHighlightedPresenter} />}
+						</>
+					}
+				</>
 			}
-			{(allPresenters && showAll) && <AllPresenters presenters={notHighlightedPresenter} />}
         </Section>
     );
 };
